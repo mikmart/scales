85816d2078903346a6c59e8bf5a504b003a76bbd
Mikko Marttila <mikmart@mikkomarttila.com>
2018-09-08 13:27:03 +0300

    Add SI scale and prefix finders

--- R/si-powers.R
+++ R/si-powers.R
@@ -21,3 +21,27 @@ si_powers <- sort(c(
   "z"      = -21L,
   "y"      = -24L
 ))
+
+si_scale <- function(x, base = 10) {
+  powers <- si_powers_of(base)
+
+  i <- findInterval(abs(x), 10^powers)
+  power <- unname(powers)[pmax(i, 1L)]
+  power[is.infinite(x) | x == 0] <- 0L
+
+  10^power
+}
+
+si_prefix <- function(x, base = 10) {
+  powers <- si_powers_of(base)
+
+  i <- findInterval(abs(x), 10^powers)
+  prefix <- names(powers)[pmax(i, 1L)]
+  prefix[is.infinite(x) | x == 0] <- ""
+
+  prefix
+}
+
+si_powers_of <- function(base) {
+  si_powers[rowSums(1 - sign(outer(si_powers, log10(base), `%%`))) > 0]
+}
